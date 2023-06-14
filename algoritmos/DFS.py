1c1d679482a2e134d0225a63d326ea260863df71
thiago <thiagoandre2121@gmail.com>
2023-06-14 19:48:52 -0300

    removing algo folder

--- algoritmos/DFS.py
+++
@@ -1,6 +0,0 @@
-def DFS(visited, graph, node):  #function for dfs 
-    if node not in visited:
-        print (node)
-        visited.add(node)
-        for neighbour in graph[node]:
-            DFS(visited, graph, neighbour)
